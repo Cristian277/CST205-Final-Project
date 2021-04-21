68f802ea4b6f48194345595c6cad39c69a3093e4
Cristian <carredondo@csumb.edu>
2021-04-21 15:26:45 -0700

    API request returns 200 Successful

--- main.py
+++ main.py
@@ -6,32 +6,22 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 boostrap = Bootstrap(app)
 
+"""
 my_key = 'c5d329154c6814866283f9572098cff2'
-
 parameters = {
   'api_key': my_key,
 }
-
-endpoint = 'https://api.themoviedb.org/3/movie'
-
-#This is going to be super important once we retrieve the correct data
-#Plan is go into the view the view will have a post form that sends the movie you're looking for
-#as a parameter to our search function we then try to look for it with the code right above
-#if it works then load the page with the results by using the code right below this.
-#also have a way to get back to home we should have nagivation
-#data = json.load(open('cs_people.json'))
+endpoint = 'https://api.themoviedb.org/3/movie/'
+"""
 
 #Home Route
 @app.route('/')
 def hello():
     try:
-        r = requests.get(endpoint, params=payload)
-        data = r.json()
-        pprint(data)
+        response = requests.get("https://api.themoviedb.org/3/movie/550?api_key=c5d329154c6814866283f9572098cff2")
+        print(response.json())
     except:
         print('please try again')
-    
-    ourData = json.load(open(data))
 
     return render_template('index.html')
 
