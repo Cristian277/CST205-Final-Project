ae803811c94a6ae6daf9eb5f42dfc38d779f8bc0
ChrJimenez <47259794+christianJimenez1999@users.noreply.github.com>
2021-04-25 16:42:56 -0700

    added some css and finished movie descriptions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests, json
 from pprint import pprint
-from flask import Flask, render_template 
+from flask import Flask, render_template, redirect, url_for, request 
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -22,10 +22,11 @@ def hello():
     listOfImages = []
     try:
         response = requests.get("https://api.themoviedb.org/3/movie/popular/?api_key=c5d329154c6814866283f9572098cff2")
-        print(response.json())
+        # print(response.json())
         data = response.json()
         for i in range(0,9):
             listOfImages.append("https://image.tmdb.org/t/p/w300" + data['results'][i]['poster_path'])
+            print( listOfImages)
     except:
         print('please try again')
 
@@ -35,7 +36,12 @@ def hello():
 #Second Page Route
 @app.route('/page2')
 def p2():
-    return render_template('page2.html')
+    title = request.args.get('title', None)
+    description = request.args.get('desc', None)
+    release = request.args.get('release', None)
+    popularity = request.args.get('popularity', None)
+    image = request.args.get('image', None)
+    return render_template('page2.html', title = title, desc = description, release = release, popularity = popularity, image = image)
 
 
 
