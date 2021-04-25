ea83601315c1e574359f08c9f29f697e6b7d2ddc
ChrJimenez <47259794+christianJimenez1999@users.noreply.github.com>
2021-04-25 16:45:13 -0700

    added my code

--- main.py
+++ main.py
@@ -1,45 +1,47 @@
 import requests, json
 from pprint import pprint
-from flask import Flask, render_template 
+from flask import Flask, render_template, redirect, url_for, request 
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 boostrap = Bootstrap(app)
 
+"""
 my_key = 'c5d329154c6814866283f9572098cff2'
-
 parameters = {
   'api_key': my_key,
 }
+endpoint = 'https://api.themoviedb.org/3/movie/'
+<img src= "'/images/ + {{data['results'][i]['poster_path']}} + '"/>
+"""
 
-endpoint = 'https://api.themoviedb.org/3/movie'
-
-#This is going to be super important once we retrieve the correct data
-#Plan is go into the view the view will have a post form that sends the movie you're looking for
-#as a parameter to our search function we then try to look for it with the code right above
-#if it works then load the page with the results by using the code right below this.
-#also have a way to get back to home we should have nagivation
-#data = json.load(open('cs_people.json'))
 
 #Home Route
 @app.route('/')
 def hello():
+    listOfImages = []
     try:
-        r = requests.get(endpoint, params=payload)
-        data = r.json()
-        pprint(data)
+        response = requests.get("https://api.themoviedb.org/3/movie/popular/?api_key=c5d329154c6814866283f9572098cff2")
+        # print(response.json())
+        data = response.json()
+        for i in range(0,9):
+            listOfImages.append("https://image.tmdb.org/t/p/w300" + data['results'][i]['poster_path'])
+            print( listOfImages)
     except:
         print('please try again')
-    
-    ourData = json.load(open(data))
 
-    return render_template('index.html')
+    return render_template('index.html',data=data, images = listOfImages)
 
 
 #Second Page Route
 @app.route('/page2')
 def p2():
-    return render_template('page2.html')
+    title = request.args.get('title', None)
+    description = request.args.get('desc', None)
+    release = request.args.get('release', None)
+    popularity = request.args.get('popularity', None)
+    image = request.args.get('image', None)
+    return render_template('page2.html', title = title, desc = description, release = release, popularity = popularity, image = image)
 
 
 
